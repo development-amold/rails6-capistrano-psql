ac1ddd13974cfaa9a674a73d7b96c85a63fbd54a
Amol Dumbre <dev.amold@gmail.com>
2020-07-12 12:35:47 +0530

    Updated for assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,23 +64,23 @@ end
   # end
   
 
-# namespace :deploy do
-#   namespace :assets do
-#     task :precompile, :roles => :web, :except => { :no_release => true } do
-#       run "mkdir -p #{fetch :releases_path}"
-#       begin
-#         from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
-#       rescue
-#         err_no = true
-#       end
-#       if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0     
-#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-#       else
-#         logger.info "Skipping asset pre-compilation because there were no asset changes"
-#       end
-#    end
-#   end
-# end
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      # run "mkdir -p #{fetch :releases_path}"
+      begin
+        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
+      rescue
+        err_no = true
+      end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0     
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+   end
+  end
+end
 
 
 
@@ -147,19 +147,3 @@ end
 #   end
 # end
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      begin
-        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
-      rescue
-        err_no = true
-      end
-      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-   end
-  end
-end
