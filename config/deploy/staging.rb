c1b7c849a7ba28c6a2053fde4c248bcbb7adbc9f
Amol Dumbre <dev.amold@gmail.com>
2020-07-14 00:49:00 +0530

    Updated deploy-staging-conf

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,11 +5,12 @@
 
 #ubuntu@ec2-15-207-19-162.ap-south-1.compute.amazonaws.com
 
-#AWS public or PRIVATE IP
+#USE AWS IP
 server "13.127.212.201", user: "ubuntu", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :deploy_to, "/home/#{fetch :user}/#{fetch :application}"
 
 
 # role-based syntax
