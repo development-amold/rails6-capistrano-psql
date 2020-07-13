4023d4d572d6ed16735aec6bad8448a352277a30
Amol Dumbre <dev.amold@gmail.com>
2020-07-14 00:04:01 +0530

    Updated files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@
 
 #ubuntu@ec2-15-207-19-162.ap-south-1.compute.amazonaws.com
 
-#AWS public IP
+#AWS public or PRIVATE IP
 server "13.127.212.201", user: "ubuntu", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
