2fa7bc5c74bcd35e6280fc0d91e8c430f60899b3
Amol Dumbre <dev.amold@gmail.com>
2020-07-17 18:53:00 +0530

    Updated IP

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,11 +9,7 @@ set :repo_url, "git@github.com:development-amold/rails6-capistrano-psql.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-
-# set :deploy_to, "/var/www/html/#{fetch :application}"
-set :deploy_to, "/usr/share/nginx/html/#{fetch :application}"
-
-
+set :deploy_to, "/var/www/html/#{fetch :application}"
 # set :use_sudo, true
 # set :passenger_restart_with_sudo, true
 
